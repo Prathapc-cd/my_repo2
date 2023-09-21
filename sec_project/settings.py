2a290ff565736050a6b3fb91dcda3beb558e95e3
Prathapc-cd <143712443+Prathapc-cd@users.noreply.github.com>
2023-09-21 12:50:34 +0530

    Update settings.py

--- sec_project/settings.py
+++ sec_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yb#3a0urdm35tjml77c9axo(_dhm(+@2f06r4m4e49_0%t2a9q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -118,9 +118,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
