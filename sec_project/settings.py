25a192445205b6a1ce933725b172b0032841d911
Prathapc-cd <143712443+Prathapc-cd@users.noreply.github.com>
2023-10-09 10:49:05 +0530

    Update settings.py

--- sec_project/settings.py
+++ sec_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yb#3a0urdm35tjml77c9axo(_dhm(+@2f06r4m4e49_0%t2a9q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['server_domain_or_IP']
 
 
 # Application definition
@@ -117,7 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+# Media files settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
