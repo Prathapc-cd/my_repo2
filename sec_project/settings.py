32bb054c1ca487ca405bb7482b5b62737cf43057
Prathapc-cd <143712443+Prathapc-cd@users.noreply.github.com>
2023-10-10 15:45:38 +0530

    Update settings.py

--- sec_project/settings.py
+++ sec_project/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root', 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    # '/var/www/static/',
+)
 
 # Media files settings
 MEDIA_URL = '/media/'
